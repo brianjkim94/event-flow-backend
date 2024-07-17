bae3bb116ddda186f7012e61ffe16eed2e876719
brianjkim94 <brianjkim94@gmail.com>
2024-07-17 01:07:24 -0400

    controllers models server.js

---
+++ controllers/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { User } = require('../models');
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ email, password: hashedPassword });
+  await user.save();
+  res.status(201).send('User registered');
+});
+
+module.exports = router;
