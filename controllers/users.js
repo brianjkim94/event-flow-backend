0c556cf38533a0582e82c2ea17877b9bbd590038
brianjkim94 <brianjkim94@gmail.com>
2024-07-20 19:03:09 -0400

    feat: routes addition to controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const { User } = require('../models');
+const isLoggedIn = require('../middleware/isLoggedIn');
 const router = express.Router();
 
 const SALT_LENGTH = 12;
 
 router.post('/signup', async (req, res) => {
     try {
+        console.log('Received signup request:', req.body);
         const userInDatabase = await User.findOne({ username: req.body.username });
         if (userInDatabase) {
             return res.status(400).json({ error: 'Username already taken.' });
         }
         const user = await User.create({
             username: req.body.username,
-            hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
+            hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH),
+            name: req.body.firstName + ' ' + req.body.lastName,
+            phoneNumber: req.body.phone,
+            location: req.body.state,
+            email: req.body.email
         });
         const token = jwt.sign(
             { username: user.username, _id: user._id },
@@ -41,6 +47,26 @@ router.post('/signin', async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
+});
+
+router.post('/', isLoggedIn, async (req, res) => {
+    const { username, password, role, name, phoneNumber, location, email } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, SALT_LENGTH);
+    const user = new User({ username, hashedPassword, role, name, phoneNumber, location, email });
+    await user.save();
+    res.status(201).send('User created');
+});
+
+router.get('/', async (req, res) => {
+    const users = await User.find();
+    res.status(200).json(users);
+});
+
+router.put('/:id', isLoggedIn, async (req, res) => {
+    const { username, password, role, name, phoneNumber, location, email } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, SALT_LENGTH);
+    const user = await User.findByIdAndUpdate(req.params.id, { username, hashedPassword, role, name, phoneNumber, location, email }, { new: true });
+    res.status(200).send('User updated');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const authRoutes = require('./controllers/auth');
 const eventRoutes = require('./controllers/events');
 const rsvpRoutes = require('./controllers/rsvp');
+const tagRoutes = require('./controllers/tags');
 const usersRoutes = require('./controllers/users');
 const isLoggedIn = require('./middleware/isLoggedIn');
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI_LOCAL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('Database error:', err));
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -27,9 +29,9 @@ app.use(session({
 }));
 app.use(flash());
 
-app.use('/auth', authRoutes);
 app.use('/events', eventRoutes);
 app.use('/rsvp', rsvpRoutes);
+app.use('/tags', tagRoutes);
 app.use('/users', usersRoutes);
 
 const PORT = process.env.PORT || 3000;
