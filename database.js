7232f851ee4449a7b34b052fa6e695275b3ea383
tflinch <tflinch16@gmail.com>
2024-07-20 13:53:07 -0400

    feat: removed database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const uri = 'mongodb://localhost:27017/eventFlow';
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-module.exports = mongoose;
