1499c1a59f6aa09065833b7d85bfd149c24a2af7
brianjkim94 <brianjkim94@gmail.com>
2024-07-23 22:42:51 -0400

    feat: updated seedData and gitignore

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const isLoggedIn = require('./middleware/isLoggedIn');
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('Database error:', err));
 
